201512b193e68ae29c0606dda2a92614a1be153c
Javi2398 <javi2398@gmail.com>
2025-11-13 20:30:50 +0100

    'avance que no funciona'

--- Programacion-de-Inteligencia-Artificial/Tema-1/Estructuras-Repetitivas.py
+++ Programacion-de-Inteligencia-Artificial/Tema-1/Estructuras-Repetitivas.py
@@ -1,50 +1,59 @@
-# 1. Escribir un programa que imprima todos los números pares entre dos números que se le pidan al usuario.
-
-primerNumero = int(input('Introduce el valor del primer número: '))
-segundoNumero = int(input('Introduce el valor del segundo número: '))
-
-for valor in range(primerNumero, segundoNumero + 1): # Con el +1 hacemos que incluya el segundo numero.
-    if valor % 2 == 0:
-        print(valor)
-
-
-# 2. Realizar un algoritmo que pida números (se pedirá por teclado la cantidad de números a introducir). El programa debe informar de cuantos números introducidos son mayores que 0, menores que 0 e iguales a 0.
-
 import random
 
+# 1. Escribir un programa que imprima todos los números pares entre dos números que se le pidan al usuario.
 
-mayoresCero = 0
-menoresCero = 0
-igualesCero = 0
+# primerNumero = int(input('Introduce el valor del primer número: '))
+# segundoNumero = int(input('Introduce el valor del segundo número: '))
 
-cantidadNumeros = int(input('Introduce la cantidad de valores que desea introducir: '))
+# for valor in range(primerNumero, segundoNumero + 1): # Con el +1 hacemos que incluya el segundo numero.
+#     if valor % 2 == 0:
+#         print(valor)
 
-for valor in range(cantidadNumeros):
-    numeroIntroducido = int(input(f'Introduce el valor número {valor}: '))
 
-    if numeroIntroducido > 0:
-        mayoresCero += 1
-    elif numeroIntroducido < 0:
-        menoresCero += 1
-    else:
-        igualesCero += 1
+# # 2. Realizar un algoritmo que pida números (se pedirá por teclado la cantidad de números a introducir). El programa debe informar de cuantos números introducidos son mayores que 0, menores que 0 e iguales a 0.
 
-print(f'Ha introducido {mayoresCero} valores mayores de 0, {menoresCero} valores menores de 0 y {igualesCero} valores iguales a 0.')
+
+
+# mayoresCero = 0
+# menoresCero = 0
+# igualesCero = 0
+
+# cantidadNumeros = int(input('Introduce la cantidad de valores que desea introducir: '))
+
+# for valor in range(cantidadNumeros):
+#     numeroIntroducido = int(input(f'Introduce el valor número {valor}: '))
+
+#     if numeroIntroducido > 0:
+#         mayoresCero += 1
+#     elif numeroIntroducido < 0:
+#         menoresCero += 1
+#     else:
+#         igualesCero += 1
+
+# print(f'Ha introducido {mayoresCero} valores mayores de 0, {menoresCero} valores menores de 0 y {igualesCero} valores iguales a 0.')
 
 
 # 3. Crea una aplicación que permita adivinar un número. La aplicación genera un número aleatorio del 1 al 100. A continuación va pidiendo números y va respondiendo si el número a adivinar es mayor o menor que el introducido, además de los intentos que te quedan (tienes 10 intentos para acertarlo). El programa termina cuando se acierta el número (además te dice en cuantos intentos lo has acertado), si se llega al limite de intentos te muestra el número que había generado.
 
-numeroAAdivinar = random.randint(1, 100)
+LOWER_VALUE = 1
+MAX_VALUE = 100
+MAX_TRIES = 10
 
-for intento in range(10):
-    numeroUsuario = int(input("Introduzca su número: "))
+random_number = random.randint(LOWER_VALUE, MAX_VALUE)
 
-    if numeroUsuario == numeroAAdivinar:
-        print('Ha adivinado el número en el intento ', intento)
-    elif numeroUsuario > numeroAAdivinar:
+for tries in range(1, MAX_TRIES+1):
+    numeroUsuario = int(input(f"Intento: {tries}. Introduzca un número del 1 al 100: "))
+
+    if numeroUsuario == random_number:
+        print('Ha adivinado el número en el intento ', tries)
+        break
+    elif numeroUsuario > random_number:
         print('El número a adivinar es menor a ', numeroUsuario)
     else:
         print('El número a adivinar es mayor a ', numeroUsuario)
+    
+    if tries == MAX_TRIES:
+        print(f'Lo siento, el numero a acertar era el {random_number}')
 
 
 # 4. Escribe un programa que pida el limite inferior y superior de un intervalo. Si el límite inferior es mayor que el superior lo tiene que volver a pedir.
